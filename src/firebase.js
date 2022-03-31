e457af145a252f5cc361581d54c92ce2a9d4589c
DevTaaki <kjtag0408@gmail.com>
2022-03-31 20:25:17 +0900

    복구

--- src/firebase.js
+++ src/firebase.js
@@ -8,7 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    
+    apiKey: "AIzaSyCojBF0K5kxGfr8RigMEdfYIH8r5KI16uM",
+    authDomain: "sparta-react-basic-a254b.firebaseapp.com",
+    projectId: "sparta-react-basic-a254b",
+    storageBucket: "sparta-react-basic-a254b.appspot.com",
+    messagingSenderId: "948196364273",
+    appId: "1:948196364273:web:7faee7796226a45665fe50",
+    measurementId: "G-KEK13SEQDG"
 };
 
 initializeApp(firebaseConfig);
